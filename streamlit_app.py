e06729e8dd820e972b4f71e9f72a4deb9b033578
Varun Adibhatla <vr00n@users.noreply.github.com>
2022-02-22 07:02:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,38 +6,38 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-def app():
-    loc_button = st.button('Get Device Location')
-    loc_button.js_on_event(
-        "button_click",
-        CustomJS(
-            code="""
-        navigator.geolocation.getCurrentPosition(
-            (loc) => {
-                document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-            }
-        )
-        """
-        ),
-    )
-    result = streamlit_bokeh_events(
-        loc_button,
-        events="GET_LOCATION",
-        key="get_location",
-        refresh_on_update=False,
-        override_height=75,
-        debounce_time=0,
-    )
 
-    if result:
-        if "GET_LOCATION" in result:
-            loc = result.get("GET_LOCATION")
-            lat = loc.get("lat")
-            lon = loc.get("lon")
-            st.write(f"Lat, Lon: {lat}, {lon}")
+loc_button = st.button('Get Device Location')
+loc_button.js_on_event(
+"button_click",
+CustomJS(
+    code="""
+navigator.geolocation.getCurrentPosition(
+    (loc) => {
+        document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+    }
+)
+"""
+),
+)
+result = streamlit_bokeh_events(
+loc_button,
+events="GET_LOCATION",
+key="get_location",
+refresh_on_update=False,
+override_height=75,
+debounce_time=0,
+)
 
-            m = leafmap.Map(center=(lat, lon), zoom=16)
-            m.add_basemap("ROADMAP")
-            popup = f"lat, lon: {lat}, {lon}"
-            m.add_marker(location=(lat, lon), popup=popup)
-            m.to_streamlit()
+if result:
+if "GET_LOCATION" in result:
+    loc = result.get("GET_LOCATION")
+    lat = loc.get("lat")
+    lon = loc.get("lon")
+    st.write(f"Lat, Lon: {lat}, {lon}")
+
+    m = leafmap.Map(center=(lat, lon), zoom=16)
+    m.add_basemap("ROADMAP")
+    popup = f"lat, lon: {lat}, {lon}"
+    m.add_marker(location=(lat, lon), popup=popup)
+    m.to_streamlit()
